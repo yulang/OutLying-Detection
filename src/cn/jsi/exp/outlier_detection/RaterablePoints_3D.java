6e110a9c13a7082eb57fe3c0352bd1ddc617a7aa
yulang <y_u_lang@163.com>
2013-10-26 11:12:24 +0800

    1

---
+++ src/cn/jsi/exp/outlier_detection/RaterablePoints_3D.java
@@ -0,0 +1,11 @@
+package cn.jsi.exp.outlier_detection;
+
+public class RaterablePoints_3D extends RaterablePoints {
+	private double z;
+	public RaterablePoints_3D(double x, double y, double z) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+		this.z=z;
+	}
+
+}
